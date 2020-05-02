c780380706d668f4e0801ac6c43c11c727b33d55
kanekotic <me@kanekotic.com>
2020-05-02 15:50:21 +0200

    #2 | Alvaro | added recording of error to logs

--- src/main/kotlin/com/wefox/kanekotic/centralizedPayments/utils/CustomProcessingExceptionHandler.kt
+++ src/main/kotlin/com/wefox/kanekotic/centralizedPayments/utils/CustomProcessingExceptionHandler.kt
@@ -1,14 +1,39 @@
 package com.wefox.kanekotic.centralizedPayments.utils
 
+import com.github.kittinunf.fuel.httpPost
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.streams.errors.ProductionExceptionHandler
+import com.github.kittinunf.result.Result
+import com.wefox.kanekotic.centralizedPayments.serdes.PaymentSerde
 
 class CustomProcessingExceptionHandler : ProductionExceptionHandler {
     override fun handle(
         record: ProducerRecord<ByteArray, ByteArray>,
         exception: Exception
     ): ProductionExceptionHandler.ProductionExceptionHandlerResponse {
-        println("PASSING")
+        val payment = PaymentSerde.get().deserializer.deserialize("", record.value())
+        val httpAsync = "http://localhost:8997/log"
+            .httpPost()
+            .header("Content-Type", "application/json")
+            .body("""{
+                | 'payment_id': '${payment.payment_id}',
+                | 'error_type': 'other',
+                | 'error_description': '${exception.message}'
+                |}""".trimMargin())
+            .responseString { _, _, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        println(data)
+                    }
+                }
+            }
+
+        httpAsync.join()
         return ProductionExceptionHandler.ProductionExceptionHandlerResponse.CONTINUE
     }
 
--- src/test/kotlin/com/wefox/kanekotic/centralizedPayments/utils/CustomProcessingExceptionHandlerTest.kt
+++ src/test/kotlin/com/wefox/kanekotic/centralizedPayments/utils/CustomProcessingExceptionHandlerTest.kt
@@ -1,19 +1,63 @@
 package com.wefox.kanekotic.centralizedPayments.utils
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import com.wefox.kanekotic.centralizedPayments.Faker
+import com.wefox.kanekotic.centralizedPayments.serdes.PaymentSerde
+import io.mockk.every
 import io.mockk.mockk
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.errors.SerializationException
 import org.apache.kafka.streams.errors.ProductionExceptionHandler
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+
 
 class CustomProcessingExceptionHandlerTest {
+    private val port = 8997
+    private val url  = "localhost"
+    private val wiremock = WireMockServer(wireMockConfig().port(port))
+
+    @BeforeEach
+    fun beforeEach() {
+        wiremock.start()
+        wiremock.resetAll()
+        configureFor(url, port)
+    }
+    @AfterEach
+    fun afterEach() {
+        wiremock.stop()
+    }
+
     @Test
-    fun shouldFailIfEmpty() {
-        val exception = mockk<Exception>(relaxed = true)
+    fun shouldHandleExceptionAndCallLog() {
+        val exception = Exception("kaboom")
         val record = mockk<ProducerRecord<ByteArray, ByteArray>>(relaxed = true)
+        val payment = Faker.payment()
+        every { record.value() } returns PaymentSerde.get().serializer.serialize("", payment)
+
+        wiremock.stubFor(post("/log")
+            .withHeader("Content-Type", equalTo("application/json"))
+            .withRequestBody(equalTo("""{
+                | 'payment_id': '${payment.payment_id}',
+                | 'error_type': 'other',
+                | 'error_description': '${exception.message}'
+                |}""".trimMargin()))
+            .inScenario("Scenario")
+            .willReturn(ok()))
         val result = CustomProcessingExceptionHandler().handle(record, exception)
         Assertions.assertEquals(ProductionExceptionHandler.ProductionExceptionHandlerResponse.CONTINUE, result)
+        wiremock.verify(
+            1,
+            postRequestedFor(urlEqualTo("/log"))
+                .withHeader("Content-Type", equalTo("application/json"))
+                .withRequestBody(equalTo("""{
+                | 'payment_id': '${payment.payment_id}',
+                | 'error_type': 'other',
+                | 'error_description': '${exception.message}'
+                |}""".trimMargin())))
     }
+
 }
