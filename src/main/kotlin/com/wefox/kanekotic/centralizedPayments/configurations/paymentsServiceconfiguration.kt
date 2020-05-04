3e91b161c7f794cc66f15a6ebc8689bd64d1c994
kanekotic <me@kanekotic.com>
2020-05-04 11:30:03 +0200

    NOISSUE | Alvaro | added configuration support for urls of services

---
+++ src/main/kotlin/com/wefox/kanekotic/centralizedPayments/configurations/paymentsServiceconfiguration.kt
@@ -0,0 +1,6 @@
+package com.wefox.kanekotic.centralizedPayments.configurations
+
+object paymentServiceConfiguration {
+    val url = FileConfig.config[payments.url]
+
+}
--- src/main/kotlin/com/wefox/kanekotic/centralizedPayments/main.kt
+++ src/main/kotlin/com/wefox/kanekotic/centralizedPayments/main.kt
@@ -1,11 +1,14 @@
 package com.wefox.kanekotic.centralizedPayments
 
+import com.wefox.kanekotic.centralizedPayments.clients.LogClient
 import com.wefox.kanekotic.centralizedPayments.clients.PaymentsClient
 import com.wefox.kanekotic.centralizedPayments.configurations.KafkaConfiguration
 import com.wefox.kanekotic.centralizedPayments.configurations.KafkaConfiguration.streamsConfig
+import com.wefox.kanekotic.centralizedPayments.configurations.LogConfiguration
 import com.wefox.kanekotic.centralizedPayments.configurations.PostgressConfiguration
-import com.wefox.kanekotic.centralizedPayments.models.Toggles
+import com.wefox.kanekotic.centralizedPayments.configurations.paymentServiceConfiguration
 import com.wefox.kanekotic.centralizedPayments.persistors.PaymentPersistor
+import com.wefox.kanekotic.centralizedPayments.processors.ErrorHandlerProcessor
 import com.wefox.kanekotic.centralizedPayments.processors.SavePaymentProcessor
 import com.wefox.kanekotic.centralizedPayments.processors.ValidatePaymentProcessor
 import com.wefox.kanekotic.centralizedPayments.serdes.PaymentSerde
@@ -33,6 +36,7 @@ class Main {
                 ).peek{ _, value -> println(value) }
 
                 SavePaymentProcessor(stream, paymentPersistor)
+                ErrorHandlerProcessor(stream, LogClient(LogConfiguration))
             }
 
             if (ToggleConfiguration.online) {
@@ -40,7 +44,7 @@ class Main {
                     KafkaConfiguration.ONLINE_INPUT_TOPIC,
                     Consumed.with(Serdes.String(), paymentSerde.serde)
                 )
-                val validatePaymentProcessor = ValidatePaymentProcessor(stream, PaymentsClient)
+                val validatePaymentProcessor = ValidatePaymentProcessor(stream, PaymentsClient(paymentServiceConfiguration))
                 SavePaymentProcessor(validatePaymentProcessor, paymentPersistor)
             }
             val streams = KafkaStreams(builder.build(), props)
