f14ef3fd1ef174fd55c25a2b64d13683f1da36d3
kanekotic <me@kanekotic.com>
2020-05-04 06:50:21 +0200

    NOISSUE | Alvaro | added configuration support

--- src/main/kotlin/com/wefox/kanekotic/centralizedPayments/configurations/PostgressConfiguration.kt
+++ src/main/kotlin/com/wefox/kanekotic/centralizedPayments/configurations/PostgressConfiguration.kt
@@ -1,5 +1,5 @@
 package com.wefox.kanekotic.centralizedPayments.configurations
 
 object PostgressConfiguration {
-    const val CONNECTION_STRING = "jdbc:postgresql://localhost:5432/payments?user=tech&password=test"
+     val CONNECTION_STRING = FileConfig.config[FileConfig.postgressConnectionString]
 }
