30b262a09466226e4b8bce2f11d6ef47204f0bbb
kanekotic <me@kanekotic.com>
2020-05-06 06:25:38 +0200

    NOISSUE | Alvaro | split in 2 diferent kafka streams providers

--- src/main/kotlin/com/wefox/kanekotic/centralizedPayments/Main.kt
+++ src/main/kotlin/com/wefox/kanekotic/centralizedPayments/Main.kt
@@ -1,12 +1,11 @@
 package com.wefox.kanekotic.centralizedPayments
 
+import com.github.kittinunf.result.Result
 import com.wefox.kanekotic.centralizedPayments.clients.LogClient
+import com.wefox.kanekotic.centralizedPayments.clients.LogResponseException
 import com.wefox.kanekotic.centralizedPayments.clients.PaymentsClient
-import com.wefox.kanekotic.centralizedPayments.configurations.KafkaConfiguration
+import com.wefox.kanekotic.centralizedPayments.configurations.*
 import com.wefox.kanekotic.centralizedPayments.configurations.KafkaConfiguration.streamsConfig
-import com.wefox.kanekotic.centralizedPayments.configurations.LogConfiguration
-import com.wefox.kanekotic.centralizedPayments.configurations.PaymentServiceConfiguration
-import com.wefox.kanekotic.centralizedPayments.configurations.PostgressConfiguration
 import com.wefox.kanekotic.centralizedPayments.models.Error
 import com.wefox.kanekotic.centralizedPayments.models.ErrorType
 import com.wefox.kanekotic.centralizedPayments.models.Payment
@@ -24,16 +23,14 @@ import org.apache.kafka.streams.kstream.Consumed
 
 class Main {
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val props = streamsConfig
-            val builder = StreamsBuilder()
+        fun onlineStream(): KafkaStreams {
+            val builderOffline = StreamsBuilder()
             val paymentSerde = PaymentSerde.get()
-            val logClient = LogClient(LogConfiguration)
             val paymentPersistor =
                 PaymentPersistor(DriverManager.getConnection(PostgressConfiguration.getConnectionString()))
-
+            val logClient = LogClient(LogConfiguration)
             if (ToggleConfiguration.offline) {
-                builder.stream(
+                builderOffline.stream(
                     KafkaConfiguration.OFFLINE_INPUT_TOPIC,
                     Consumed.with(Serdes.String(), paymentSerde.serde)
                 )
@@ -41,8 +38,22 @@ class Main {
                     .errorHandlerProcessor(logClient)
             }
 
+            val streamsOffline = KafkaStreams(builderOffline.build(), streamsConfig)
+            streamsOffline.setUncaughtExceptionHandler { _, e ->
+                logClient.logError(Payment("00000", 0, "online", "00000", 0.0, 0), Error(ErrorType.other, e.message!!))
+                System.exit(1)
+            }
+            return streamsOffline
+
+        }
+        fun offlineStream(): KafkaStreams {
+            val builderOnline = StreamsBuilder()
+            val paymentSerde = PaymentSerde.get()
+            val logClient = LogClient(LogConfiguration)
+            val paymentPersistor =
+                PaymentPersistor(DriverManager.getConnection(PostgressConfiguration.getConnectionString()))
             if (ToggleConfiguration.online) {
-                builder.stream(
+                builderOnline.stream(
                     KafkaConfiguration.ONLINE_INPUT_TOPIC,
                     Consumed.with(Serdes.String(), paymentSerde.serde)
                 )
@@ -50,31 +61,44 @@ class Main {
                     .savePaymentProcessor(paymentPersistor)
                     .errorHandlerProcessor(logClient)
             }
-
-            val streams = KafkaStreams(builder.build(), props)
-            val latch = CountDownLatch(1)
-
-            streams.setUncaughtExceptionHandler { _, e ->
+            val streamsOnline = KafkaStreams(builderOnline.build(), streamsConfig)
+            streamsOnline.setUncaughtExceptionHandler { _, e ->
                 logClient.logError(Payment("00000", 0, "online", "00000", 0.0, 0), Error(ErrorType.other, e.message!!))
                 System.exit(1)
             }
+            return streamsOnline
+        }
+
+        @JvmStatic fun main(args: Array<String>) {
+
+            val streamsOnline = onlineStream()
+            val streamsOffline =offlineStream()
+            val latch = CountDownLatch(1)
 
             Runtime.getRuntime().addShutdownHook(object : Thread("streams-centralized-payments-shutdown-hook") {
                 override fun run() {
-                    streams.close()
+                    streamsOffline.close()
+                    streamsOnline.close()
                     latch.countDown()
                 }
             })
-            try {
-                println("starting...")
-                streams.start()
-                latch.await()
-            } catch (e: Throwable) {
-                System.exit(1)
-            }
 
-            println("closing...")
-            System.exit(0)
+            Result.of<Unit, Exception> {
+                println("starting...")
+                if(ToggleConfiguration.offline) {
+                    streamsOffline.start()
+                }
+                if(ToggleConfiguration.online) {
+                    streamsOnline.start()
+                }
+                latch.await()
+            }.fold({
+                println("closing...")
+                System.exit(0)
+            },{
+                System.exit(1)
+            })
+
         }
     }
 }
